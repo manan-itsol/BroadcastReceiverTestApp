9f8b4d03a251a480fdaf82ab08542a46595884a9
manan-itsol <abdulmannan9669@gmail.com>
2021-12-04 14:12:11 +0500

    Add project files.

---
+++ BroadcastReceiverTestApp/BroadcastReceiverTestApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using BroadcastReceiverTestApp.ViewModels;
+using BroadcastReceiverTestApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BroadcastReceiverTestApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ BroadcastReceiverTestApp/BroadcastReceiverTestApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BroadcastReceiverTestApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BroadcastReceiverTestApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
